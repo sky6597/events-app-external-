d46d168e3b5a64bc941f676c18d3184988af535a
Akash Kumar Shaw <40659414+sky6597@users.noreply.github.com>
2022-02-18 16:23:10 +0530

    update title

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Welcome to Infinity Cloud application.</h1>"));
+        chai.assert.isTrue(res.text.includes("<h1>Welcome to Infinity Cloud application. Demo</h1>"));
         return done();
       });
 
